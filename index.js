28ee0f0924dd1a1c0a973b06f13779beacca06b2
thomalexg <thomas.gae@posteo.de>
2021-01-22 14:13:00 +0100

    Done, created my own image with a top and bottom text

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const fs = require('fs');
 const Jimp = require('jimp');
 
-try {
-  if (!fs.existsSync('./YourOwnMeme')) {
-    fs.mkdirSync('./YourOwnMeme');
-  }
-} catch (err) {
-  console.error(err);
-}
+// https://i.pinimg.com/originals/2a/02/06/2a02060f84e3eb0a900386ed87788fa0.jpg
+
 const createMeme = async () => {
-  const image = await Jimp.read(
-    'https://i.pinimg.com/originals/2a/02/06/2a02060f84e3eb0a900386ed87788fa0.jpg',
-  );
+  const url = process.argv[2];
+
+  let top = '';
+  let bottom = '';
+
+  if (process.argv[3]) {
+    top = process.argv[3].split('.').join(' ');
+  }
+  if (process.argv[4]) {
+    bottom = process.argv[4].split('.').join(' ');
+  }
+
+  // const top = process.argv[3].split('.').join(' ');
+
+  // const bottom = process.argv[4].split('.').join(' ');
+
+  const image = await Jimp.read(url);
   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  const print = await image.print(font, 0, 0, 'Hello Meme');
+  const print = await image.print(font, 510, 0, top);
+  const print2 = await image.print(font, 0, 350, bottom);
+
   image.write('./YourOwnMeme/YourImage.png');
 };
 createMeme();
